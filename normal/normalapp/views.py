e3064849eb0161d0c2b2e18bb91c3cdb51a48143
rakinplaban <rakinshahriar54@gmail.com>
2021-08-25 01:36:51 +0600

    changed

--- normal/normalapp/views.py
+++ normal/normalapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+import json
 # Create your views here.
+
+
+def index(request):
+    with open("stock_market_data.json", "r") as readit:
+        jsonobject = json.load(readit)
+    
+        return render(request,"normalapp/index.html",{
+            "jsonobject" : jsonobject
+        })
